ff9d4ecf634cfea53ba34542a3259a6e182b9030
Shruti-jain23 <145157293+Shruti-jain23@users.noreply.github.com>
2026-03-06 20:16:11 +0530

    Update app.py
    visual

--- backend/app.py
+++ backend/app.py
@@ -15,24 +15,54 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+os.makedirs("temp", exist_ok=True)
+
+last_output_path = None
+
+
 @app.get("/")
 def home():
     return {"message": "DeepCacher API running"}
 
+
 @app.post("/compress")
 async def compress_file(file: UploadFile = File(...)):
-
-    os.makedirs("temp", exist_ok=True)
+    global last_output_path
 
     input_path = f"temp/{file.filename}"
 
     with open(input_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    # run compression
     output_path = compressor.compress(input_path)
+    last_output_path = output_path
+
+    # stats
+    original_size = os.path.getsize(input_path)
+    compressed_size = os.path.getsize(output_path)
+
+    compression_ratio = round(
+        (1 - (compressed_size / original_size)) * 100, 2
+    )
+
+    return {
+        "filename": file.filename,
+        "original_size": original_size,
+        "compressed_size": compressed_size,
+        "compression_ratio": compression_ratio
+    }
+
+
+@app.get("/download")
+def download_file():
+    global last_output_path
+
+    if not last_output_path:
+        return {"error": "No file compressed yet"}
 
     return FileResponse(
-        output_path,
-        filename="compressed_" + file.filename + ".deepcacher",
+        last_output_path,
+        filename=os.path.basename(last_output_path),
         media_type="application/octet-stream"
     )
